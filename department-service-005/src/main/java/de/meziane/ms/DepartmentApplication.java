ae5209cdba7dafda506ef026ab240597b227701a
Meziano <me.kettou@gmail.com>
2019-05-30 18:30:28 +0200

    First commit

---
+++ department-service-005/src/main/java/de/meziane/ms/DepartmentApplication.java
@@ -0,0 +1,14 @@
+package de.meziane.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@EnableFeignClients
+@SpringBootApplication
+public class DepartmentApplication {
+    public static void main( String[] args ) {
+    	SpringApplication.run(DepartmentApplication.class, args);
+    }
+}
